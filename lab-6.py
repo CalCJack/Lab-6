36e49c9baff4c1fee7d467e8404526e920e01a18
BuildTools <unconfigured@null.spigotmc.org>
2023-03-08 19:32:32 -0500

    Added working decode function

--- lab-6.py
+++ lab-6.py
@@ -1,3 +1,4 @@
+#Caleb Jackson
 def main():
     print('Menu')
     print('-------------')
@@ -23,7 +24,22 @@ def encode(raw_pass:list):
     return encoded
 
 def decode(password:list):
-    pass # for the code to be added by Alexander
+
+    #Cycles through each digit of the encoded password and reduces the value by 3 to decode it.
+    original = ""
+    for i in password:
+        if int(i) > 2:
+            original = original + str(int(i) - 3)
+
+        #The following if statements are used so that the digits wrap back around instead of going into negative numbers
+        elif i == "2":
+            original = original + "9"
+        elif i == "1":
+            original = original + "8"
+        else:
+            original = original + "7"
+
+    return original
 
 running, password = True, None
 if __name__ == '__main__':
