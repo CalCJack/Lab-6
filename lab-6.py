c2a43a45ebda7f62d6dc8abc9b545b5d2455e839
CalCJack <calebjackson@ufl.edu>
2023-03-08 20:40:45 -0500

    Final Version for Grading

--- lab-6.py
+++ lab-6.py
@@ -10,13 +10,16 @@ def main():
 def encode(raw_pass:list):
     encode = ''
     for i in raw_pass:
-        if int(i) > 6: # taking into account the size of the number we can filter out ones that take a little extra to change
+        if int(i) > 6:
+            # taking into account the size of the number we can filter out ones that take a little extra to change
             i = ((int(i) + 3) - 10)
         else:
-            i = int(i) + 3 # reassigining it to i just for simplicity sake
+            i = int(i) + 3
+            # reassigining it to i just for simplicity sake
         encode += str(i)
     print('Your password has been encoded and stored!')
     return encode
+    # returning the encoded value back to the user
 
 def decode(password:list):
 
@@ -38,13 +41,21 @@ def decode(password:list):
 
 running, password = True, None
 if __name__ == '__main__':
+    #Starting the loop that will run for the duration of the program
     while running:
         option = main()
+        # User chooses to encode it and it is stored
         if option == 1:
             password = encode((input('Please enter your password to encode:')))
+        # User chooses to decode and it is attempted, if it fails the dialogue bounces back
         elif option == 2:
-            print(f'The encoded password is {password}, and the original password is {decode(password)}')
+            try:
+                print(f'The encoded password is {password}, and the original password is {decode(password)}')
+            except:
+                print(f"No password to decode!")
+        # Using try and except lets the program avoid breaking
         elif option == 3:
             running = False
+        # A condition to help the code from being broken by a wrong input
         else:
             print('Error! Invalid Input.')
